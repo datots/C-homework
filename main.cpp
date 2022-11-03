080fa80bb692a42d92bad67d0b919e61d44f2b8f
David Tsanava <dato_ts@hotmail.com>
2022-11-03 17:33:30 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      char symbol = 'T';
+  cout << symbol << endl;
+  
+  int num1 = 100;
+  cout << num1 << endl;
+  
+  
+  float num2 = 10.10;
+  cout << num2 << endl;
+  
+  
+  double num3 = 50.23;
+  cout << num3 << endl;
+  int number = 5;
+  cout<<number<<endl;
+  int array[5] = {5, 10, 15, 20, 30};
+  int sum = array[0] + array[1] + array[2] + array[3] + array[4]; 
+  cout << sum << endl;
+  int average = sum / number;
+  cout << average << endl;
+  cout << "ჯამი: " << sum << endl;
+  cout << "საშუალო არითმეტიკული: " << average << endl;
+
+    return 0;
+}
